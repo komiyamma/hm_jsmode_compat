9a6824454187bd47d908b9a2a3bdb969ef62e05e
komiyamma <komiyama0629@gmail.com>
2022-09-11 18:17:23 +0900

    バージョンを1.01へ

--- build/hidemarucompat.js
+++ build/hidemarucompat.js
@@ -1,9 +1,21 @@
 /*
  * This codes is licensed under CC0 1.0 Universal
  *
- * hidemarucompat v1.0.0
+ * hidemarucompat v1.0.1
  */
 (function () {
+    var guid = "{BA97AD4E-1AF7-457A-AFE5-E270E0212A70}";
+    var op_dllobj = null;
+    function output(msg) {
+        if (!op_dllobj) {
+            op_dllobj = hidemaru.loadDll(hidemaruGlobal.hidemarudir() + "\\HmOutputPane.dll");
+        }
+        if (op_dllobj) {
+            var msg_replaced = msg.replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
+            return op_dllobj.dllFunc.Output(hidemaruGlobal.hidemaruhandle(0), msg_replaced);
+        }
+        return false;
+    }
     var gvm = function (s) { return hidemaru.getVar(s); };
     var evm = function (s) { return hidemaru.evalMacro(s); };
     var hidemacJsGlobalStatements = "var s=m+' ';for(var i=0;i<arguments.length;i++){if(i>0)s+=',';var a=arguments[i];if(typeof(a)=='string'){s+='R\"\xFF('+a+')\xFF\"';}else{s+=a;}}evm(s+';###=result;');var r=gvm('###');";
@@ -41,6 +53,15 @@
         module.exports = hc;
     }
     else {
+        if (typeof (hidemaruCompat) != 'undefined') {
+            if (hidemaruCompat.guid == null || hidemaruCompat.guid != guid) {
+                output("本モジュールとは異なるhidemaruCompatが、すでに定義されています。\r\n上書きします。\r\n");
+            }
+            // 一致していたら上書きはしない
+            if (hidemaruCompat.guid == guid) {
+                return;
+            }
+        }
         hidemaruCompat = hc;
     }
 })();
