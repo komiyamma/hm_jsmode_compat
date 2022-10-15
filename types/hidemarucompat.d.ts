58d5eebb922cfc63b4dfb253edd59a871d5e7612
komiyamma <komiyama0629@gmail.com>
2022-10-15 09:28:30 +0900

    requireのv1.0.5での変更にあわせた修正

--- types/hidemarucompat.d.ts
+++ types/hidemarucompat.d.ts
@@ -2,7 +2,7 @@
  * @file 秀丸のjsmode用のTypeScript定義ファイル
  * @author Akitsugu Komiyama
  * @license This typescript definition file is licensed under CC0 1.0 Universal
- * @version v1.0.3
+ * @version v1.0.4
  */
 
  declare namespace hidemaruCompat {
